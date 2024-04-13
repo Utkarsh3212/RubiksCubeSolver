d59db18d37820cd3793292d722a5a51a67039806
Utkarsh3212 <utkarshsingh25703@gmail.com>
2024-04-14 03:04:02 +0530

    Update the files of the basic structure of cube.

--- CubeModel/RubiksCube.h
+++ CubeModel/RubiksCube.h
@@ -5,6 +5,10 @@
 #ifndef RUBIKSCUBESOLVER_RUBIKSCUBE_H
 #define RUBIKSCUBESOLVER_RUBIKSCUBE_H
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
 /* A base class for representation of all our Rubiks Cube Models.
  *Rubiks Cube can be modelled in several different representation with each of them
  *having its own special definitons. This class provides a shared functionality
@@ -35,6 +39,89 @@ class RubiksCube {
             U,UPRIME,U2,
             B,BPRIME,B2
         };
+
+        virtual COLOUR getColour(FACE face,unsigned row,unsigned col) const=0;
+
+        static char getColourLetter(COLOUR colour);
+
+        virtual bool isSolved() const=0;
+
+        static string getMove(MOVE ind);
+
+        /*
+         *the cube is laid out as follows:
+         * side wise:
+         *      U
+         *    L F R B
+         *      D
+         *
+         *  colour wise:
+         *
+         *          W W W
+         *          W W W
+         *          W W W
+         *
+         *  G G G   R R R   B B B   O O O
+         *  G G G   R R R   B B B   O O O
+         *  G G G   R R R   B B B   O O O
+         *
+         *          Y Y Y
+         *          Y Y Y
+         *          Y Y Y
+         *
+         * Row and Column Numberings:
+         * rx -> row numbering
+         * cx -> column numbering
+         * bx -> both row and column numbering
+         *
+         *             b0 c1 c2
+         *             r1  .  .
+         *             r2  .  .
+         *
+         *  b0 c1 c2   b0 c1 c2   b0 c1 c2   b0 c1 c2
+         *  r1  .  .   r1  .  .   r1  .  .   r1  .  .
+         *  r2  .  .   r2  .  .   r2  .  .   r2  .  .
+         *
+         *             b0 c1 c2
+         *             r1  .  .
+         *             r2  .  .
+         *
+        */
+        void print() const;
+
+        vector<MOVE> randomCubeShuffle(unsigned int times);
+
+        RubiksCube &move(MOVE ind);
+
+        RubiksCube &invert(MOVE ind);
+
+        virtual RubiksCube &f()const=0;
+        virtual RubiksCube &fPrime()const=0;
+        virtual RubiksCube &f2()const=0;
+
+        virtual RubiksCube &r()const=0;
+        virtual RubiksCube &rPrime()const=0;
+        virtual RubiksCube &r2()const=0;
+
+        virtual RubiksCube &b()const=0;
+        virtual RubiksCube &bPrime()const=0;
+        virtual RubiksCube &b2()const=0;
+
+        virtual RubiksCube &d()const=0;
+        virtual RubiksCube &dPrime()const=0;
+        virtual RubiksCube &d2()const=0;
+
+        virtual RubiksCube &l()const=0;
+        virtual RubiksCube &lPrime()const=0;
+        virtual RubiksCube &l2()const=0;
+
+        virtual RubiksCube &u()const=0;
+        virtual RubiksCube &uPrime()const=0;
+        virtual RubiksCube &u2()const=0;
+
+        string getCornerColourString(uint8_t ind) const;
+        uint8_t getCornerColour(uint8_t ind) const;
+        uint8_t getCornerOrientation(uint8_t) const;
 };
 
 
