b2d90abe322946ef2ae905c0f57605f284b94308
vantablack <utkarshsingh25703@gmail.com>
2024-04-21 17:40:04 +0530

    IDA* Solver and CornerPatternDatabase successfully tested.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include "Solver/DFSSolver.h"
 #include "Solver/BFSSolver.h"
 #include "Solver/IDDFSSolver.h"
-#include "CubeModel/RubiksCube3dArray.cpp"
-#include "CubeModel/RubiksCube1dArray.cpp"
-#include "CubeModel/RubiksCubeBitboard.cpp"
+#include "Solver/IDAstarSolver.h"
+//#include "CubeModel/RubiksCube3dArray.cpp"
+//#include "CubeModel/RubiksCube1dArray.cpp"
+//#include "CubeModel/RubiksCubeBitboard.cpp"
+#include "PatternDatabases/CornerDBMaker.h"
+
 
 using namespace std;
 
@@ -72,7 +75,7 @@ int main()
 
 //    RubiksCubeBitboard cube;
 //    cube.print();
-//
+
 //    vector<RubiksCube::MOVE> shuffle_moves = cube.randomCubeShuffle(8);
 //    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
 //    cout << "\n";
@@ -83,5 +86,53 @@ int main()
 //    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
 //    cout<<'\n';
 //    iddfsSolver.rubiksCube.print();
+
+//    CornerPatternDatabase Testing----------------------------------------------------------------------------------------------------------------------------------------------
+
+//    CornerPatternDatabase cornerDB(0xFF);
+//    RubiksCubeBitboard cube;
+//    cube.print();
+
+//    cout<<cornerDB.getDatabaseIndex(cube)<<'\n';
+//    cornerDB.setNumMoves(cube, 2);
+//    cout<<cornerDB.getDatabaseIndex(cube)<<'\n';
+//    cout<<cornerDB.setNumMoves(cube, 5)<<'\n';
+
+
+
+//    cube.randomCubeShuffle(1);
+//    cube.print();
+//    cout << (int)cornerDB.getNumMoves(cube) << "\n";
+//
+//    cornerDB.setNumMoves(cube, 6);
+//
+//    cout << (int)cornerDB.getNumMoves(cube) << "\n";
+//
+//    cout<<(int)cornerDB.getNumItems()<<'\n';
+//
+//    cout<<(int)(cornerDB.getNumMoves(22747861))<<'\n';
+
+//    IDA*-SOLVER Testing and Database generation----------------------------------------------------------------------------------------------------------------------------------------------
+
+    string filename="/home/vantablack/CLionProjects/RubiksCubeSolver/Databases/DBv3.txt";
+
+//    CornerDBMaker cornerDb(filename,0xFF);
+//    cornerDb.bfsAndStore();
+    RubiksCube1dArray cube;
+    cube.print();
+    auto shuffleMoves = cube.randomCubeShuffle(13);
+    cube.print();
+    for (auto move: shuffleMoves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+
+    IDAstarSolver<RubiksCube1dArray, Hash1d> idaStarSolver(cube, filename);
+    auto moves = idaStarSolver.solve();
+
+    idaStarSolver.rubiksCube.print();
+    for (auto move: moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+
+
+    return 0;
 }
 
